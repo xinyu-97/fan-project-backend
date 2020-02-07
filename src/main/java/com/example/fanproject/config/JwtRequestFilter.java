b4ff2b26ae4bc8fcc4af5df93b315805d3f3ee3f
Xinyu Chen <xinyuchen2769@gmail.com>
2020-02-07 16:15:00 -0500

    fix the CORS problem

--- src/main/java/com/example/fanproject/config/JwtRequestFilter.java
+++ src/main/java/com/example/fanproject/config/JwtRequestFilter.java
@@ -63,6 +63,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
+
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
--- src/main/java/com/example/fanproject/controller/FanController.java
+++ src/main/java/com/example/fanproject/controller/FanController.java
@@ -3,12 +3,14 @@ package com.example.fanproject.controller;
 import com.example.fanproject.model.Fan;
 import com.example.fanproject.service.FanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class FanController {
     @Autowired
